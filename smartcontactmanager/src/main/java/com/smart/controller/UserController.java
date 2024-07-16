20fc7c177b2c55b22ffce7dc39cbd1ffc5f24bac
MY <MY@DESKTOP-REI99IL>
2024-07-16 16:38:38 +0530

    Integrate TinyMCE Rich text Editor Display saved contacts on a page and apply pagination on it

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -6,15 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +28,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.smart.entity.Contact;
 import com.smart.entity.User;
 import com.smart.helper.Message;
+import com.smart.repositories.ContactRepository;
 import com.smart.repositories.UserRepository;
 
 @Controller
@@ -32,6 +38,8 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
@@ -97,5 +105,22 @@ public class UserController {
 		}
 		
 		return "normal/add_contact_form";
+	}
+	
+	//pagination per page 5, current page -0
+	@GetMapping("/show-contacts/{page}")
+	public String showContact(@PathVariable("page") int page,  Model m, Principal principal) {
+		m.addAttribute("title", "show user contact");
+		//contact list
+		String userName = principal.getName();
+		User user = userRepository.getUSerByUserName(userName);
+		int id = user.getId();
+		Pageable pageable = PageRequest.of(page, 3);
+		Page<Contact> contacts = contactRepository.findContactsByUser(id,pageable);
+		m.addAttribute("contacts", contacts);
+		m.addAttribute("currentPage", page);
+		m.addAttribute("totalPages",contacts.getTotalPages());
+		
+		return "normal/show_contacts";
 	}
 }
